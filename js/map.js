d54bdf13cab178eafc6754c7ed83cc811a0a4594
Santi Díaz <kerbindr@gmail.com>
2016-08-17 18:50:20 -0400

    Se coloco la informacion correspondiente en 'Servicios', 'Diferentes peces' y algunos ajustes

--- js/map.js
+++ js/map.js
@@ -1,8 +1,17 @@
 var map;
 
 function initMap() {
+  var miUbicacion = {lat: 18.468345, lng: -69.849891};
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 18.468345, lng: -69.849891},
+    center: miUbicacion,
     zoom: 16
   });
+
+  var marker = new google.maps.Marker({
+    position: miUbicacion,
+    map: map,
+    title: 'El Grupo #3 estuvo aquí'
+  });
+
 }
